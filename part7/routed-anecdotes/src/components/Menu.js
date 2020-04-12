db9a506e16377337a069887a3de0860d69c69f2a
Valtteri Kodisto <valtteri.kodisto@reaktor.com>
2020-04-12 18:03:59 +0300

    Added 7.1 - 7.3

---
+++ part7/routed-anecdotes/src/components/Menu.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import React from "react"
+
+const Menu = () => {
+  const padding = {
+    paddingRight: 5,
+  }
+  return (
+    <div>
+      <Link to="/" style={padding}>
+        anecdotes
+      </Link>
+      <Link to="/create" style={padding}>
+        create new
+      </Link>
+      <Link to="/about" style={padding}>
+        about
+      </Link>
+    </div>
+  )
+}
+
+export default Menu
